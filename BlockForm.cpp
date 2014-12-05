0735fb43736a4e2c6308d499e418238fff93fb38
mannsi <mannsi1983@gmail.com>
2014-12-05 18:07:29 +0000

    Forgot to add BlockFrom.cpp

---
+++ BlockForm.cpp
@@ -0,0 +1,20 @@
+#include "BlockForm.h"
+
+using namespace std;
+
+vector<Point*>  BlockForm::_worldVector() {
+    vector<Point*> pointsToReturn;
+    for (int i = 0; i < form.size(); ++i) {
+        vector<bool> lineVector = form[i];
+        for (int j = 0; j < lineVector.size(); ++j) {
+            if (lineVector[j])
+            {
+                Point* worldPoint = new Point(initialPoint);
+                worldPoint->X -= j;
+                worldPoint->Y -= i;
+                pointsToReturn.push_back(worldPoint);
+            }
+        }
+    }
+    return pointsToReturn;
+}
